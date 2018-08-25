d9587f48dce2f1e0be6e8ae1412ca1f42ce6046f
Aarish Adeel <aarish8@gmail.com>
2018-08-25 04:58:08 -0400

    Sound testing

--- index.js
+++ index.js
@@ -1,6 +1,75 @@
-//p5 code goes here
-function setup() {
+const record = require('node-record-lpcm16');
 
-  createCanvas(200, 500);
-  background(0);
-}
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+// const encoding = 'Encoding of the audio file, e.g. LINEAR16';
+// const sampleRateHertz = 16000;
+// const languageCode = 'BCP-47 language code, e.g. en-US';
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: false, // If you want interim results, set this to true
+};
+
+// Create a recognize stream
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on('error', console.error)
+  .on('data', data =>
+    process.stdout.write(
+      data.results[0] && data.results[0].alternatives[0] ?
+      `Transcription: ${data.results[0].alternatives[0].transcript}\n` :
+      `\n\nReached transcription time limit, press Ctrl+C\n`
+    )
+  );
+
+// Start recording and send the microphone input to the Speech API
+record
+  .start({
+    sampleRateHertz: sampleRateHertz,
+    threshold: 0,
+    // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
+    verbose: false,
+    recordProgram: 'rec', // Try also "arecord" or "sox"
+    silence: '10.0',
+  })
+  .on('error', console.error)
+  .pipe(recognizeStream);
+
+console.log('Listening, press Ctrl+C to stop.');
+
+// let mic, voiceRecorder, soundFile, button;
+//
+// function setup() {
+//   noCanvas();
+//
+//   mic = new p5.AudioIn();
+//   mic.start();
+//
+//   soundFile = new p5.SoundFile();
+//
+//   button = createButton("Start/Stop Recording");
+//   button.mouseReleased(recordVoice);
+// }
+//
+// function recordVoice() {
+//   voiceRecorder = new p5.SoundRecorder();
+//   voiceRecorder.setInput(mic);
+//   voiceRecorder.record(soundFile);
+//   setTimeout(() => {
+//     voiceRecorder.stop();
+//     soundFile.play();
+//     saveSound(soundFile, "voiceRecording.wav");
+//   }, 5000);
+// }
