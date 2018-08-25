f26b68d0b3c48bb5e7f5da7b79e070f02b214c43
JekkuChen <Joeyjunyichen@hotmail.ca>
2018-08-25 05:33:02 -0400

    Aasd

--- drawContent.js
+++ drawContent.js
@@ -1,9 +1,9 @@
-function drawContent(Page=-1){
+function drawContent(Page) {
   let page = Page;
-
   removeElements();
 
-  if(page == -1){
+
+  if (page == -1) {
     //test page
     let inbox;
     let outbox;
@@ -43,14 +43,14 @@ function drawContent(Page=-1){
     outbox.elt.rows = "4";
     outbox.parent(divOutbox);
 
-    function recStart(inbox){
+    function recStart() {
       //start recording here
       let input;
       input = inbox.value();
       console.log(input);
     }
 
-    function recStop(){
+    function recStop() {
       //stop recording, save, put in text to speech, paste into output
       let output;
       outbox.value(output);
@@ -59,7 +59,7 @@ function drawContent(Page=-1){
 
   }
 
-  if(page == 0){
+  if (page == 0) {
     let divHeader;
     let div_paragraph;
     let divButton;
@@ -73,19 +73,67 @@ function drawContent(Page=-1){
     header = createElement("h1", "placeholder");
     header.parent(divHeader);
 
+    divButton = createDiv();
+    divButton.parent("content");
+
+    recStartB = createButton("Start");
+    recStartB.position (625, 450);
+    recStartB.mouseReleased(recStart);
+    recStartB.parent(divButton);
+
+
+    function recStart() {
+      //start recording here
+      removeElements();
+      drawContent(1);
+    }
+
   }
-  if(page == 1){
-    // paste speech
+  if (page == 1) {
+    let inbox;
+
+    let divInbox;
+    let divButton;
+
+  
+    divInbox = createDiv();
+    divInbox.parent("content");
+
+    inbox = createElement("textarea");
+    inbox.position(350,200);
+    inbox.elt.placeholder = "input goes here";
+    inbox.elt.cols = "80";
+    inbox.elt.rows = "15";
+    inbox.parent(divInbox);
+
+    divButton = createDiv();
+    divButton.parent("content");
+
+    recStartB = createButton("Enter");
+    recStartB.position (625, 450);
+    recStartB.mouseReleased(recStart);
+    recStartB.parent(divButton);
+
+
+    function recStart() {
+      //start recording here
+      let input;
+      input = inbox.value();
+      console.log(input); //For Testing Purposes
+      removeElements();
+      drawContent(2);
+    }
+
+
   }
-  if(page == 2){
+  if (page == 2) {
     // record self
   }
-  if(page == 3){
+  if (page == 3) {
     // processing
   }
-  if(page == 4){
+  if (page == 4) {
     // review
   }
-
 
 }
