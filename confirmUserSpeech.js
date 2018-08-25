07740148663412aed00bb89ebf73a5ab53eaa6e4
Raymond <raymond101.qu@gmail.com>
2018-08-25 13:03:11 -0400

    Initial speech and text checking.

---
+++ confirmUserSpeech.js
@@ -0,0 +1,13 @@
+let example1 = "I want to be able to say this.";
+let example2 = "I went to be able to nay say this.";
+
+function parseSpeech(userSentenceString, userOriginalText) {
+  // check the user's original text with what they said
+  let userSentenceStringArr = userSentenceString.split(" ");
+  console.log(userSentenceStringArr);
+
+  for (int i = 0; i < userSentenceStringArr.length; i++) {
+    //check each index
+    if
+  }
+}
