82a142665b3b1ee0c6c624f902218ec81f54d720
Bill Li <bill.l1564262@gmail.com>
2018-08-25 01:17:26 -0400

    Created code skeleton

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-//p5 code goes here
-function setup() {
+let inbox;
+let outbox;
+let b_rec_start;
+let b_rec_stop;
 
-  createCanvas(200, 500);
-  background(0);
+let input;
+let output;
+
+function setup() {
+  noCanvas();
+  inbox = createElement("textarea");
+  inbox.elt.placeholder = "input goes here";
+  inbox.elt.cols = "50";
+  inbox.elt.rows = "4";
+  inbox.html("yeet");
+
+  b_rec_start = createButton("record");
+  b_rec_start.mouseReleased(rec_start);
+
+  b_rec_stop = createButton("stop recording");
+  b_rec_start.mouseReleased(rec_stop);
+
+  outbox = createElement("textarea");
+  outbox.elt.placeholder = "output goes here";
+  outbox.elt.cols = "50";
+  outbox.elt.rows = "4";
+
+  //background(0);
+}
+
+
+function rec_start(){
+  input = inbox.value();
+  console.log(input);
+
+  //start recording here
+}
+
+function rec_stop(){
+  //stop recording, save, put in text to speech, paste into output
+
+  outbox.value(output);
+  console.log(output);
 }
