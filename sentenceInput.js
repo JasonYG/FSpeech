c07a75e63728c6dcdd9ad45dfeea5e136abf0f83
Raymond <raymond101.qu@gmail.com>
2018-08-25 01:22:09 -0400

    Skeleton to draw user input on screen.

---
+++ sentenceInput.js
@@ -0,0 +1,7 @@
+function sentenceInput() {
+  // replace testSentence with user input
+  var testSentence = "This is a test sentence.";
+  // console.log("Sentence input called");
+
+  return testSentence;
+}
