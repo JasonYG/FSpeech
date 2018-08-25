542e3b1e12f9fcc16e09d3ada70cf8ebf786dcfe
Raymond <raymond101.qu@gmail.com>
2018-08-25 14:51:54 -0400

    User speech algo update.

--- confirmUserSpeech.js
+++ confirmUserSpeech.js
@@ -1,11 +1,20 @@
 let example1 = "I want to be able to say this.";
 let example2 = "I went to be able to nay say this.";
 
-function parseSpeech(userSentenceString, userOriginalText) {
+//testing
+
+
+function checkSpeech(userSentenceString, userOriginalText) {
+  if (userSentenceString == userOriginalText){
+    console.log("Perfect speaking!");
+  }
+
   // check the user's original text with what they said
   let userSentenceStringArr = userSentenceString.split(" ");
-  console.log(userSentenceStringArr);
+  let userOriginalTextArr = userOriginalText.split(" ");
 
+  // both arrays are different lengths
+  if (userSentenceStringArr)
   for (int i = 0; i < userSentenceStringArr.length; i++) {
     //check each index
     if
