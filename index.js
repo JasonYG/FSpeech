0caecfa235374d78ee73eb9d6c53d12b17b2b079
Jason Guo <jasonyzguo@gmail.com>
2018-08-25 04:32:37 -0400

    Added example nodejs for speech api

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-//p5 code goes here
-function setup() {
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
 
-  createCanvas(200, 500);
-  background(0);
-}
+// Your Google Cloud Platform project ID
+const projectId = 'eb65461e5da074fa8cd9920972f372f952614848';
+
+// Creates a client
+const client = new speech.SpeechClient({
+  projectId: projectId,
+});
+
+// The name of the audio file to transcribe
+const fileName = './sample_audio/audio.raw';
+
+// Reads a local audio file and converts it to base64
+const file = fs.readFileSync(fileName);
+const audioBytes = file.toString('base64');
+
+// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+const audio = {
+  content: audioBytes,
+};
+const config = {
+  encoding: 'LINEAR16',
+  sampleRateHertz: 16000,
+  languageCode: 'en-US',
+};
+const request = {
+  audio: audio,
+  config: config,
+};
+
+// Detects speech in the audio file
+client
+  .recognize(request)
+  .then(data => {
+    const response = data[0];
+    const transcription = response.results
+      .map(result => result.alternatives[0].transcript)
+      .join('\n');
+    console.log(`Transcription: ${transcription}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
